71eb245f0e9c8ffe7963317699d53ca685da7369
VvsJaemin <djd453216@nate.com>
2022-12-24 16:43:08 +0900

    templateMethod 패턴

---
+++ src/templatemethod/TemplateMethodMain.java
@@ -0,0 +1,15 @@
+package templatemethod;
+
+public class TemplateMethodMain {
+    public static void main(String[] args) {
+        AbstractDisplay d1 = new CharDisplay('H');
+
+        AbstractDisplay d2 = new StringDisplay("Hello, World");
+
+        //d1, d2 모두 같은 AbstractDisplay의 하위 클래스의 인스턴스이므로
+        //상속한 display의 메소드를 호출 할 수 있다.
+        //실제 동작은 하위 클래스에서 정해진다.
+        d1.display();
+        d2.display();
+    }
+}
