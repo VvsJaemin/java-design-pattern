79f5afded7c58401ed231d545887e96b8fec8081
VvsJaemin <djd453216@nate.com>
2023-01-02 23:57:46 +0900

    Prototype 패턴(1)

---
+++ src/prototype/PrototypeMain.java
@@ -0,0 +1,31 @@
+package prototype;
+
+import prototype.framework.Manager;
+import prototype.framework.Product;
+
+public class PrototypeMain {
+    public static void main(String[] args) {
+        //준비
+        Manager manager = new Manager();
+        UnderlinePen upen = new UnderlinePen('-');
+        MessageBox mbox = new MessageBox('*');
+        MessageBox sbox = new MessageBox('/');
+
+
+        //등록
+        manager.register("Strong Message", upen );
+        manager.register("Waring Box", mbox );
+        manager.register("Slash Box", sbox );
+
+        //생성과 사용
+        Product product = manager.create("Strong Message");
+        product.use("강한 메시지");
+
+        Product p2 = manager.create("Waring Box");
+        p2.use("경고 박스");
+
+        Product p3 = manager.create("Slash Box");
+        p3.use("슬래쉬 박스");
+
+    }
+}
--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -1,21 +1,21 @@
 package singleton;
 // 여러 인스턴스를 생성하기 위한 속도를 일부러 느리게 하는 메서드 생성
 public class Singleton {
-    private static Singleton singleton = new Singleton();
+    private static Singleton singleton = null;
 
     public Singleton() {
         System.out.println("하나의 인스턴스를 생성했습니다.");
         slowDown();
     }
 
-    public static Singleton getInstance() {
-        if (singleton == null) {
+    public static synchronized Singleton getInstance() { //엄격한 싱글턴 패턴 적용(synchornized)
+        if (singleton == null) { // 조건 판단이 엄격하지 않기 때문에(스레드 세이프) 싱글턴이 아님
             singleton = new Singleton();
         }
         return singleton;
     }
 
-    private void slowDown() {
+    private void slowDown() { // 초기화를 지연 시키는 지연 초기화
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
