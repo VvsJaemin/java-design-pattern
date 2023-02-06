90c60c57f30474b0144bb787e3c2302b35a84416
VvsJaemin <djd453216@nate.com>
2023-02-06 23:02:42 +0900

    Mediator 패턴

---
+++ src/mediator/ColleagueCheckbox.java
@@ -0,0 +1,29 @@
+package mediator;
+
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class ColleagueCheckbox extends Checkbox implements ItemListener, Colleague {
+    private Mediator mediator;
+
+    public ColleagueCheckbox(String caption, CheckboxGroup group, boolean state) throws HeadlessException {
+        super(caption, group, state);
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        //상태가 변화하면 Mediator에 알린다
+        mediator.colleagueChanged();
+    }
+
+    @Override
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @Override
+    public void setColleagueEnabled(boolean enabled) {
+        setEnabled(enabled);
+    }
+}
