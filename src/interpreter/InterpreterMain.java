c3218dfd521d73507e705382a6a3d14250302afa
VvsJaemin <djd453216@nate.com>
2023-02-27 23:19:09 +0900

    Interpreter 패턴

---
+++ src/interpreter/InterpreterMain.java
@@ -0,0 +1,20 @@
+package interpreter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class InterpreterMain {
+    public static void main(String[] args) {
+        try {
+            for (String text : Files.readAllLines(Path.of("program.txt"))) {
+                System.out.println("text = " + text);
+                Node node = new ProgramNode();
+                node.parse(new Context(text));
+                System.out.println("node = " + node);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
